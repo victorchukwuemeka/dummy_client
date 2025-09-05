32432d77fe04acdbc6f7b420ef7c01de48b55b6a
viktr <chukwuemekavictor693@gmail.com>
2025-09-05 11:46:34 +0100

    structuring-the-crappy-rpc

---
+++ src/gossip.rs
@@ -0,0 +1,6 @@
+use anyhow::Result;
+
+pub async fn start()->Result<()>{
+     println!("👂 Listening to Solana gossip (not yet implemented)...");
+    Ok(())
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
-fn main() {
-    println!("Hello, world!");
+mod gossip;
+mod rpc;
+
+#[tokio::main]
+async fn main()->anyhow::Result<()>{
+    println!("Dummy client started");
+    let _gossip = gossip::start().await?;
+    let peers = rpc::fetch_gossip().await?;
+    print!("Found peers from {}", peers.len());
+
+    for peer in peers.iter().take(5){
+        println!(
+            "Peer: {} | Gossip: {:?} |Tpu: {:?} | RPC: {:?}",
+            peer.pubkey, peer.gossip, peer.tpu , peer.rpc
+        );
+    }
+
+    Ok(())
 }
